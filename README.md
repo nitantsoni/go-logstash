f714b6fe827271a78b707ff947cb979f48c1e8e3
nitantsoni <nitantsoni@gmail.com>
2022-08-29 21:00:03 +0000

    Clean up and stripdown code -Updated Readme -Timeout functionality removed to allow for persistent connections -Removed DNS Resolution from connect() -Stripped down code and removed unnecessary functions such as Dump & New

--- README.md
+++ README.md
@@ -3,21 +3,28 @@ go-logstash
 
 A golang package that sends newline delimited data via tcp to logstash.
 
+This fork has been updated from the source repo, inorder to have a persistent TCP connection. Several other changes were done mainly to simplify the code and remove superflous code.
+
 Example
 -------
 ```go
 import(
-	"github.com/heatxsink/go-logstash"
+	"github.com/nitantsoni/go-logstash"
 	"fmt"
 )
 
 func main() {
-	l := logstash.New("127.0.0.1", 9000, 5)
+	host := net.TCPAddr {
+        	IP:net.ParseIP("127.0.0.1"),
+        	Port:9000,
+	}
+	l := &logstash.Logstash{Host:host,Connection:nil,}
 	_, err := l.Connect()
 	if err != nil {
 		fmt.Println(err)
 	}
-	err = l.Writeln("{ 'foo' : 'bar' }")
+	//Writeln takes []byte arrays instead of strings
+	err = l.Writeln([]byte("{ 'foo' : 'bar' }"))
 	if err != nil {
 		fmt.Println(err)
 	}
--- logstash.go
+++ logstash.go
@@ -2,85 +2,36 @@ package logstash
 
 import (
 	"net"
-	"fmt"
 	"time"
 	"errors"
 )
 
 type Logstash struct {
-	Hostname string
-	Port int
+	Host net.TCPAddr
 	Connection *net.TCPConn
-	Timeout int
-}
-
-func New(hostname string, port int, timeout int) *Logstash {
-	l := Logstash{}
-	l.Hostname = hostname
-	l.Port = port
-	l.Connection = nil
-	l.Timeout = timeout
-	return &l
-}
-
-func (l *Logstash) Dump() {
-	fmt.Println("Hostname:   ", l.Hostname)
-	fmt.Println("Port:       ", l.Port)
-	fmt.Println("Connection: ", l.Connection)
-	fmt.Println("Timeout:    ", l.Timeout)
-}
-
-func (l *Logstash) SetTimeouts() {
-	deadline := time.Now().Add(time.Duration(l.Timeout) * time.Millisecond)
-	l.Connection.SetDeadline(deadline)
-	l.Connection.SetWriteDeadline(deadline)
-	l.Connection.SetReadDeadline(deadline)
 }
 
 func (l *Logstash) Connect() (*net.TCPConn, error) {
-	var connection *net.TCPConn
-	service := fmt.Sprintf("%s:%d", l.Hostname, l.Port)
-	addr, err := net.ResolveTCPAddr("tcp", service)
-	if err != nil {
-		return connection, err
-	}
-	connection, err = net.DialTCP("tcp", nil, addr)
-	if err != nil {
-		return connection, err
-	}
+	connection, err := net.DialTCP("tcp", nil, &l.Host)
 	if connection != nil {
 		l.Connection = connection
-		l.Connection.SetLinger(0) // default -1
-		l.Connection.SetNoDelay(true)
 		l.Connection.SetKeepAlive(true)
 		l.Connection.SetKeepAlivePeriod(time.Duration(5) * time.Second)
-		l.SetTimeouts()
 	}
 	return connection, err
 }
 
-func (l *Logstash) Writeln(message string) (error) {
-	var err = errors.New("TCP Connection is nil.")
-	message = fmt.Sprintf("%s\n", message)
+func (l *Logstash) Writeln(message []byte) (error) {
 	if l.Connection != nil {
-		_, err = l.Connection.Write([]byte(message))
+		_, err := l.Connection.Write(message)
 		if err != nil {
-			if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
-				l.Connection.Close()
-				l.Connection = nil
-				if err != nil {
-					return err
-				}
-			} else {
-				l.Connection.Close()
-				l.Connection = nil
-				return err
-			}
+			l.Connection.Close()
+			l.Connection = nil
+			return err
 		} else {
-			// Successful write! Let's extend the timeoul.
-			l.SetTimeouts()
+			// Successful write!
 			return nil
 		}
 	}
-	return err
+	return errors.New("TCP Connection is nil.")
 }
